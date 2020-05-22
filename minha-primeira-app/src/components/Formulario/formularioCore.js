4e933c30686c9df8d3551195cd7c93fdbfe38bcd
jessicaRRSantos <jessica.rr.santos@gmail.com>
2020-05-21 21:04:53 -0300

    alteração de título de core

--- minha-primeira-app/src/components/Formulario/formularioCore.js
+++ minha-primeira-app/src/components/Formulario/formularioCore.js
@@ -6,7 +6,7 @@ import 'rc-collapse/assets/index.css';
 const htmlFormularioCore = () => {
     return (
         <Collapse accordion={true}>
-            <Panel header="DADOS PESSOAIS" headerClassName="FormTitle">
+            <Panel header="CORE" headerClassName="FormTitle">
                 <fieldset>
                     <div className="col-2">
                         <div>
